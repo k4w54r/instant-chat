8e221c08bb71018ef80fdfd517b5cf1e88330999
Kawsar <k4w54r13@gmail.com>
2020-12-26 17:00:16 +0600

    Added basic functions for io

--- server/index.js
+++ server/index.js
@@ -9,6 +9,14 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+io.on('connection', (socket) => {
+  console.log('New connection');
+
+  socket.on('disconnect', () => {
+    console.log('User left');
+  });
+});
+
 app.use(router);
 
 server.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
