c6571e6b924f873c7e3846d6cc4ec3421db64ce0
Kawsar <k4w54r13@gmail.com>
2020-12-26 22:31:43 +0600

    Added Stylings

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,6 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import queryString from 'query-string';
-import io from 'socket.io-client';
+import io from "socket.io-client";
+
+import TextContainer from '../TextContainer/TextContainer';
+import Messages from '../Messages/Messages';
+import InfoBar from '../InfoBar/InfoBar';
+import Input from '../Input/Input';
+
+import './Chat.css';
+
+const ENDPOINT = 'https://project-chat-application.herokuapp.com/';
 
 let socket;
 
@@ -10,51 +19,50 @@ const Chat = ({ location }) => {
   const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = 'localhost:5000';
+
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
 
     socket = io(ENDPOINT);
 
     setRoom(room);
-    setName(name);
+    setName(name)
 
     socket.emit('join', { name, room }, (error) => {
-      if (error) {
+      if(error) {
         alert(error);
       }
     });
   }, [ENDPOINT, location.search]);
-
+  
   useEffect(() => {
-    socket.on('message', (message) => {
-      setMessages((messages) => [...messages, message]);
+    socket.on('message', message => {
+      setMessages(messages => [ ...messages, message ]);
     });
-
-    socket.on('roomData', ({ users }) => {
+    
+    socket.on("roomData", ({ users }) => {
       setUsers(users);
     });
-  }, []);
+}, []);
 
   const sendMessage = (event) => {
     event.preventDefault();
 
-    if (message) {
+    if(message) {
       socket.emit('sendMessage', message, () => setMessage(''));
     }
-  };
+  }
 
   return (
-    <div className='outerContainer'>
-      <div className='container'>
-        <input
-          value={message}
-          onChange={(event) => setMessage(event.target.value)}
-          onKeyPress={(event) => (event.key === 'Enter' ? sendMessage : null)}
-        />
+    <div className="outerContainer">
+      <div className="container">
+          <InfoBar room={room} />
+          <Messages messages={messages} name={name} />
+          <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
       </div>
+      <TextContainer users={users}/>
     </div>
   );
-};
+}
 
 export default Chat;
--- server/index.js
+++ server/index.js
@@ -1,38 +1,31 @@
+const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
-const http = require('http');
+const cors = require('cors');
 
-const PORT = process.env.PORT || 5000;
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 
 const router = require('./router');
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
+app.use(cors());
+app.use(router);
 
-io.on('connection', (socket) => {
-  console.log('New connection');
-
+io.on('connect', (socket) => {
   socket.on('join', ({ name, room }, callback) => {
     const { error, user } = addUser({ id: socket.id, name, room });
 
-    if (error) return callback(error);
+    if(error) return callback(error);
 
     socket.join(user.room);
 
-    socket.emit('message', {
-      user: 'admin',
-      text: `${user.name}, welcome to room ${user.room}.`,
-    });
-    socket.broadcast
-      .to(user.room)
-      .emit('message', { user: 'admin', text: `${user.name} has joined!` });
+    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.`});
+    socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
 
-    io.to(user.room).emit('roomData', {
-      room: user.room,
-      users: getUsersInRoom(user.room),
-    });
+    io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
 
     callback();
   });
@@ -48,19 +41,11 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => {
     const user = removeUser(socket.id);
 
-    if (user) {
-      io.to(user.room).emit('message', {
-        user: 'Admin',
-        text: `${user.name} has left.`,
-      });
-      io.to(user.room).emit('roomData', {
-        room: user.room,
-        users: getUsersInRoom(user.room),
-      });
+    if(user) {
+      io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
+      io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
     }
-  });
+  })
 });
 
-server.listen(process.env.PORT || 5000, () =>
-  console.log(`Server has started.`)
-);
+server.listen(process.env.PORT || 5000, () => console.log(`Server has started.`));
